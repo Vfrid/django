f2717c9a4aa0abb9bf7fb728cb6a07beef55fe32
Vfrid <113192881+Vfrid@users.noreply.github.com>
2023-08-03 09:30:02 -0400

    fhdlkas

--- build.sh
+++ build.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
+pip install --upgrade pip
+pip install poetry==1.2.0
+rm poetry.lock
+poetry lock
+python -m poetry install
 
-poetry install
+
 
 python manage.py collectstatic --no-input
 python manage.py migrate
